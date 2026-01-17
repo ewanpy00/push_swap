1d313274ab19db45f5f468dc40d3180d748da198
Ivan Pykhtin <ipykhtin@lab1r5s7.42abudhabi.ae>
2026-01-17 14:07:56 +0400

    draft

--- algorithm.c
+++ algorithm.c
@@ -1,6 +1,56 @@
 #include "push_swap.h"
 
-void count_cost();
+void to_stack_b(t_stack **stack_a, t_stack **stack_b);
+
+void to_stack_a(t_stack **stack_a, t_stack **stack_b);
+
+void set_pos(t_stack *stack_a, t_stack *stack_b, t_chunk *chunk){
+    int i;
+    t_stack *tmp; 
+
+    i = 0;
+    tmp = stack_a;
+    while(tmp){
+        if(stack_a->index <= chunk_counter * chunks_length)
+        {
+            if(i <= stack_a->size / 2)
+                tmp->pos = i;
+            else
+                tmp->pos = i - stack_a->size;
+        }
+        tmp = tmp->next;
+        i++;
+    }    
+}
+
+void cost(t_stack *stack_a, t_stack *stack_b, t_chunk *chunk)
+{
+    t_stack *tmp;
+
+    tmp = stack_a;
+    while(tmp)
+    {
+        if(stack_a->pos <= stack_a->size / 2)
+            cost = pos;
+        else
+            cost = pos-size;
+        tmp = tmp->next;
+    }
+}
+void set_chunks(t_stack *stack_a, t_stack *stack_b, int total_size){
+    t_chunk chunk;
+
+    if(size >= 0 && size <= 20)
+        chunk->chunks_amount = 2;
+    else if(size > 20 && size <= 100)
+        chunk->chunks_amount = 5; // 4 =5 
+    else if(size > 100 && size <= 500)
+        chunk->chunks_amount = 10; //5 - 10
+    else if(size > 500)
+        chunk->chunks_amount = 20; // ??
+    chunk->chunks_length = size / chunk.chunks_amount;
+    chunk->chunk_counter = 1;
+}
 
 void set_indexes(t_stack **stack_a)
 {
--- push_swap.h
+++ push_swap.h
@@ -8,13 +8,28 @@
 
 typedef struct s_stack
 {
+    int             size; //возможно стоит вынести из структуры но пока что так
     int             value;
     int             index;
     int             pos;
-    int             target_pos;
+    // int             target_pos;
     int             cost_a;
     int             cost_b;
     struct s_stack  *next;
 } t_stack;
+
+// typedef struct s_chunk
+// {
+//     int chunk_start;
+//     int chunks_end;
+// } t_chunk
+
+typedef struct s_chunk
+{
+    // t_chunk chunk;   
+    int chunk_counter;
+    int chunk_length;
+    int chunk_amount;
+} t_chunk;
 
 #endif
