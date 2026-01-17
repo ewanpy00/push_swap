5441ba52e3bae2674db2e8ff139afc7d298f47c3
Ivan Pykhtin <ipykhtin@lab1r5s7.42abudhabi.ae>
2026-01-17 17:20:23 +0400

    push_to_stack_B

--- algorithm.c
+++ algorithm.c
@@ -1,57 +1,28 @@
 #include "push_swap.h"
 
-void to_stack_b(t_stack **stack_a, t_stack **stack_b);
-
-void to_stack_a(t_stack **stack_a, t_stack **stack_b);
-
-void set_pos(t_stack *stack_a, t_stack *stack_b, t_chunk *chunk){
-    int i;
-    t_stack *tmp; 
-
-    i = 0;
-    tmp = stack_a;
-    while(tmp){
-        if(stack_a->index <= chunk_counter * chunks_length)
-        {
-            if(i <= stack_a->size / 2)
-                tmp->pos = i;
-            else
-                tmp->pos = i - stack_a->size;
-        }
-        tmp = tmp->next;
-        i++;
-    }    
-}
-
-void cost(t_stack *stack_a, t_stack *stack_b, t_chunk *chunk)
+// Проверяем, принадлежит ли элемент текущему чанку
+int in_chunk(t_stack *node, t_chunk *chunk)
 {
-    t_stack *tmp;
+    if (chunk->chunk_counter == 1)
+        return (node->index <= chunk->chunk_counter * chunk->chunk_length);
+    return (node->index > (chunk->chunk_counter - 1) * chunk->chunk_length &&
+            node->index <= chunk->chunk_counter * chunk->chunk_length);
+}
 
-    tmp = stack_a;
-    while(tmp)
+
+// Проверяем, есть ли в стеке элементы текущего чанка
+int chunk_in_stack(t_stack *a, t_chunk *chunk)
+{
+    while (a)
     {
-        if(stack_a->pos <= stack_a->size / 2)
-            cost = pos;
-        else
-            cost = pos-size;
-        tmp = tmp->next;
+        if (in_chunk(a, chunk))
+            return 1;
+        a = a->next;
     }
-}
-void set_chunks(t_stack *stack_a, t_stack *stack_b, int total_size){
-    t_chunk chunk;
-
-    if(size >= 0 && size <= 20)
-        chunk->chunks_amount = 2;
-    else if(size > 20 && size <= 100)
-        chunk->chunks_amount = 5; // 4 =5 
-    else if(size > 100 && size <= 500)
-        chunk->chunks_amount = 10; //5 - 10
-    else if(size > 500)
-        chunk->chunks_amount = 20; // ??
-    chunk->chunks_length = size / chunk.chunks_amount;
-    chunk->chunk_counter = 1;
+    return 0;
 }
 
+// Устанавливаем индексы элементов (0..N-1)
 void set_indexes(t_stack **stack_a)
 {
     t_stack *current;
@@ -74,30 +45,195 @@ void set_indexes(t_stack **stack_a)
     }
 }
 
-int main(){
-    int arr[] = {3, 1, 2, 5, 4, 6, 0, 8, 7, 9};
-    t_stack *stack_a = NULL;
-    t_stack *new_node;
-    for (int i = 0; i < 10; i++)
+// Находим ближайший элемент текущего чанка и вычисляем pos
+t_stack *find_closest_in_chunk(t_stack *a, t_chunk *chunk)
+{
+    int i = 0;
+    int size = a->size;
+    t_stack *tmp = a;
+
+    while (tmp)
     {
-        new_node = (t_stack *)malloc(sizeof(t_stack));
-        new_node->value = arr[i];
-        new_node->next = stack_a;
-        stack_a = new_node;
-    }
-    set_indexes(&stack_a);
-    t_stack *temp = stack_a;
-    int index = 0;
-    while (temp)
-    {
-        if(temp->index == index)
+        if (in_chunk(tmp, chunk))
         {
-            printf("Index %d: Value %d\n", temp->index, temp->value);
-            index++;
-            temp = stack_a;
+            if (i <= size / 2)
+                tmp->pos = i;
+            else
+                tmp->pos = i - size;
+            return tmp;
         }
-        else
-            temp = temp->next;
+        tmp = tmp->next;
+        i++;
     }
+    return NULL;
+}
+
+// Настройка: делим на чанки и расставляем индексы
+void setup(t_stack **a, int total_size, t_chunk *chunk)
+{
+    // Определяем количество чанков
+    if (total_size <= 20)
+        chunk->chunk_amount = 2;
+    else if (total_size <= 100)
+        chunk->chunk_amount = 5;
+    else if (total_size <= 500)
+        chunk->chunk_amount = 10;
+    else
+        chunk->chunk_amount = 20;
+
+    chunk->chunk_length = total_size / chunk->chunk_amount;
+    chunk->chunk_counter = 1;
+
+    set_indexes(a);
+}
+
+// Основная функция: перенос всех элементов из A в B
+void push_to_b(t_stack **a, t_stack **b, t_chunk *chunk)
+{
+    t_stack *target;
+    int moved;
+
+    // Проходим по всем чанкам
+    while (chunk->chunk_counter <= chunk->chunk_amount)
+    {
+        moved = 0;
+
+        while (chunk_in_stack(*a, chunk))
+        {
+            target = find_closest_in_chunk(*a, chunk);
+            if (!target)
+                break;
+
+            // Вращаем A, чтобы target оказался на вершине
+            while (target->pos > 0)
+            {
+                ra(a);
+                target->pos--;
+            }
+            while (target->pos < 0)
+            {
+                rra(a);
+                target->pos++;
+            }
+
+            // Переносим элемент в B
+            pb(a, b);
+            moved++;
+
+            // Для нижней половины чанка делаем rb
+            if ((*b)->index < (chunk->chunk_counter * chunk->chunk_length
+                               - chunk->chunk_length / 2)
+                && (moved <= chunk->chunk_length / 2))
+                rb(b);
+        }
+        // Переходим к следующему чанку
+        chunk->chunk_counter++;
+    }
+}
+
+
+
+// int main(){
+//     int arr[] = {3, 1, 2, 5, 4, 6, 0, 8, 7, 9};
+//     t_stack *stack_a = NULL;
+//     t_stack *new_node;
+//     for (int i = 0; i < 10; i++)
+//     {
+//         new_node = (t_stack *)malloc(sizeof(t_stack));
+//         new_node->value = arr[i];
+//         new_node->next = stack_a;
+//         stack_a = new_node;
+//     }
+//     set_indexes(&stack_a);
+//     t_stack *temp = stack_a;
+//     int index = 0;
+//     while (temp)
+//     {
+//         if(temp->index == index)
+//         {
+//             printf("Index %d: Value %d\n", temp->index, temp->value);
+//             index++;
+//             temp = stack_a;
+//         }
+//         else
+//             temp = temp->next;
+//     }
+//     return 0;
+// }
+
+void print_stack(char *name, t_stack *s)
+{
+    printf("%s: ", name);
+    while (s)
+    {
+        printf("%d[%d] ", s->value, s->index);
+        s = s->next;
+    }
+    printf("\n");
+}
+
+
+t_stack *new_node(int value, int index, int size)
+{
+    t_stack *n = malloc(sizeof(t_stack));
+    n->value = value;
+    n->index = index;
+    n->size = size;
+    n->next = NULL;
+    return n;
+}
+
+t_stack *build_stack(int *arr, int *idx, int size)
+{
+    t_stack *head = NULL;
+    t_stack *tmp;
+
+    for (int i = size - 1; i >= 0; i--)
+    {
+        tmp = new_node(arr[i], idx[i], size);
+        tmp->next = head;
+        head = tmp;
+    }
+    return head;
+}
+
+
+int main(void)
+{
+    int arr[] = {9, 8, 5, 2, 6, 7, 1, 3, 4, 10};
+    int size = 10;
+
+    t_stack *a = NULL;
+    t_stack *b = NULL;
+
+    // build stack
+    for (int i = size - 1; i >= 0; i--)
+    {
+        t_stack *n = malloc(sizeof(t_stack));
+        n->value = arr[i];
+        n->size = size;
+        n->next = a;
+        a = n;
+    }
+
+    set_indexes(&a);
+
+    t_chunk chunk;
+    if (size <= 20)
+        chunk.chunk_amount = 2;
+    else
+        chunk.chunk_amount = 5;
+    chunk.chunk_length = size / chunk.chunk_amount;
+    chunk.chunk_counter = 1;
+
+    print_stack("A start", a);
+    print_stack("B start", b);
+
+    push_to_b(&a, &b, &chunk);
+
+    print_stack("A end", a);
+    print_stack("B end", b);
+
     return 0;
 }
+
--- push_swap.h
+++ push_swap.h
@@ -1,35 +1,42 @@
 #ifndef PUSH_SWAP_H
-#define PUSH_SWAP_H
+# define PUSH_SWAP_H
 
-#include <stdlib.h>
-#include <unistd.h>
-#include <limits.h>
-#include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
 
 typedef struct s_stack
 {
-    int             size; //возможно стоит вынести из структуры но пока что так
-    int             value;
-    int             index;
-    int             pos;
-    // int             target_pos;
-    int             cost_a;
-    int             cost_b;
+    int             size;       
+    int             value;      
+    int             index;      
+    int             pos;       
+    int             cost_a;    
+    int             cost_b;    
     struct s_stack  *next;
-} t_stack;
-
-// typedef struct s_chunk
-// {
-//     int chunk_start;
-//     int chunks_end;
-// } t_chunk
+}   t_stack;
 
 typedef struct s_chunk
 {
-    // t_chunk chunk;   
     int chunk_counter;
-    int chunk_length;
-    int chunk_amount;
-} t_chunk;
+    int chunk_length;    
+    int chunk_amount;    
+}   t_chunk;
+
+void    sa(t_stack **a);
+void    sb(t_stack **b);
+void    ss(t_stack **a, t_stack **b);
+
+void    pa(t_stack **a, t_stack **b);
+void    pb(t_stack **a, t_stack **b);
+
+void    ra(t_stack **a);
+void    rb(t_stack **b);
+void    rr(t_stack **a, t_stack **b);
+
+void    rra(t_stack **a);
+void    rrb(t_stack **b);
+void    rrr(t_stack **a, t_stack **b);
+
 
 #endif
